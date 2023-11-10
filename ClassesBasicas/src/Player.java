26a6fe7f5fd70aee936bbf2d808c12b4481e47a2
Jacintones <139819387+Jacintones@users.noreply.github.com>
2023-11-09 23:02:37 -0300

    Update Player.java

--- ClassesBasicas/src/Player.java
+++ ClassesBasicas/src/Player.java
@@ -1,16 +1,34 @@
-public class Player extends Personagem {
-
-    private String nome;
+public class Player extends Personagem{
+    private int speed;
     private boolean vivo;
+    private Inventario inventario;
 
-    public Player(String nome) {
-        super();
-        this.nome = nome;
-        this.vivo = true;
+    public Player(String nome, int hp, int dano, int defesa) {
+        super(nome, hp, dano, defesa);
+        this.inventario = inventario;
     }
 
-    public void atacar(Inimigo inimigo) {
-        inimigo.setVida(inimigo.getVida() - this.getAtaque());
+    public int getSpeed() {
+        return speed;
     }
 
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    public Inventario getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(Inventario inventario) {
+        this.inventario = inventario;
+    }
 }
