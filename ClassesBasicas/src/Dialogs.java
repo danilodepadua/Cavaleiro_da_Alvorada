8271cdc8f72d50e4b5141dcb020ed60fea46c769
Daniel Ramos de Oliveira Tenório Bomfim <daniel-danitenorio@hotmail.com>
2023-11-11 00:54:55 -0300

    NovasClassesBasicas

---
+++ ClassesBasicas/src/Dialogs.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Dialogs extends Events{
+
+	private ArrayList<String> Falas = new ArrayList<String>();
+	
+	public Dialogs (ArrayList<String> Dialogos) {
+		this.Falas = Dialogos;
+	}
+	@Override
+	public void StartEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- ClassesBasicas/src/Inventario.java
+++ ClassesBasicas/src/Inventario.java
@@ -9,16 +9,26 @@ public class Inventario {
         this.itens = new ArrayList<>();
     }
 
-    public void adicionarItem(Item item) {
-        this.itens.add(item);
+    public void adicionarItem(Item item, int Quant) {
+        Item i = ProcurarItens(item);
+        if(i != null) {
+        	i.addQuantidade(Quant)
+        }
+        else {
+        	item.addQuantidade(Quant);
+        	this.itens.add(item);
+        }
     }
 
     public void removerItem(Item item) {
         this.itens.remove(item);
     }
-
-    public List<Item> getItens() {
-        return this.itens;
+    public Item ProcurarItens(Item item) {
+    	for(Item i : this.Itens) {
+    		if(item.Nome == i.Nome) {
+    			return i;
+    		}
+    	}
+    	return null;
     }
-
 }
