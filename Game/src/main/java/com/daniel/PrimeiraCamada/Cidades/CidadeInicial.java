d6c918fc9a6757b4578d62c9ffbc64f95cded02f
Daniel Ramos de Oliveira Tenório Bomfim <daniel-danitenorio@hotmail.com>
2024-01-12 01:13:24 -0300

    Correção de bug em 1 inimigo

--- Game/src/main/java/com/daniel/PrimeiraCamada/Cidades/CidadeInicial.java
+++ Game/src/main/java/com/daniel/PrimeiraCamada/Cidades/CidadeInicial.java
@@ -2,13 +2,13 @@ package com.daniel.PrimeiraCamada.Cidades;
 
 import com.daniel.PrimeiraCamada.Cidade;
 import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoAbelha;
-import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoSlime;
+import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoBabySlime;
 import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoTigerMan;
 import com.daniel.PrimeiraCamada.Inimigo;
 
 public class CidadeInicial extends Cidade {
     public CidadeInicial(){
         super("Cidade Inicial", "/com.daniel.Images/CidadeNoturna.jpg", "/com.daniel.Images/Fundos/FundoBatalhaBosque.jpg");
-        this.inimigos = new Inimigo[]{new InimigoSlime(), new InimigoAbelha(), new InimigoTigerMan()};
+        this.inimigos = new Inimigo[]{new InimigoBabySlime(), new InimigoAbelha(), new InimigoTigerMan()};
     }
 }
