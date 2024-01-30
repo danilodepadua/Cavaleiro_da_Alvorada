8ea7ecc88440cde1c658d0f05ed35002f839855b
Ryan Harmes Eskinazi <ryan.eskinazi@ufrpe.br>
2024-01-30 18:43:43 -0300

    Cidade - Campo de Batalha de Pedraveira

---
+++ Game/src/main/java/com/daniel/PrimeiraCamada/Cidades/BatalhaDePedraveira.java
@@ -0,0 +1,51 @@
+package com.daniel.PrimeiraCamada.Cidades;
+
+import com.daniel.PrimeiraCamada.Cidade;
+import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoAbelha;
+import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoCobraGigante;
+import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoCorvoGigante;
+import com.daniel.PrimeiraCamada.Entidades.Inimigos.InimigoLadrao;
+import com.daniel.PrimeiraCamada.Exceptions.PlayerInexistenteException;
+import com.daniel.PrimeiraCamada.Inimigo;
+import com.daniel.PrimeiraCamada.Itens.Armaduras.Capacetes.CapaceteMalha;
+import com.daniel.PrimeiraCamada.Itens.Armaduras.Peitorais.PeitoralFerro;
+import com.daniel.PrimeiraCamada.Itens.Armas.KatanaFogo;
+import com.daniel.PrimeiraCamada.Itens.Armas.KatanaGelo;
+import com.daniel.PrimeiraCamada.Itens.Armas.Tridente;
+import com.daniel.PrimeiraCamada.Itens.Pocoes.Cerveja;
+
+import java.util.ArrayList;
+
+public class BatalhaDePedraveira extends Cidade {
+    public BatalhaDePedraveira() throws PlayerInexistenteException{
+        super("Campo da Batalha de Pedraveira", "/com.daniel.Images/Fundos/BatalhaDePedraveira.jpg", "/com.daniel.Images/Fundos/FundoBatalhaBosque.jpg");
+
+        this.inimigos = new Inimigo[]{new InimigoCorvoGigante(), new InimigoCobraGigante(), new InimigoAbelha()};
+
+        this.itens.add(new Tridente());
+        this.itens.add(new KatanaGelo());
+        this.itens.add(new KatanaFogo());
+        this.itens.add(new PeitoralFerro());
+        this.itens.add(new CapaceteMalha());
+        this.itens.add(new Cerveja());
+
+        this.dialogoAtivo = true;
+        this.dialogoCutscene = "Pedraveira, a cidade eternamente marcada pelas cicatrizes de uma batalha antiga entre humanos e demônios, " +
+                "onde as ruas silenciosas contam histórias de coragem e resistência entre escombros e sombras.";
+        ajustarBotoes();
+
+
+    }
+
+    @Override
+    public void ajustarBotoes() throws PlayerInexistenteException {
+        this.botoes = new ArrayList<>();
+        this.botoes.add(criarBotaoViajar());
+        this.botoes.add(criarBotaoCacar());
+        this.botoes.add(criarBotaoLoja());
+        this.botoes.add(criarBotaoQuest());
+        this.botoes.add(criarBotaoTaverna());
+        this.botoes.add(criarBotaoSalvar());
+
+    }
+}
--- Game/src/main/java/com/daniel/PrimeiraCamada/Entidades/Player.java
+++ Game/src/main/java/com/daniel/PrimeiraCamada/Entidades/Player.java
@@ -67,6 +67,7 @@ public class Player extends Personagem implements Serializable {
         this.cidadesConehcidas.add(new CidadeMorta());
         this.cidadesConehcidas.add(new Ilha());
         this.cidadesConehcidas.add(new CidadePortuaria());
+        this.cidadesConehcidas.add(new BatalhaDePedraveira());
         this.inventario.adicionarItem(new Ferro());
         this.inventario.adicionarItem(new Ferro());
         this.inventario.adicionarItem(new Ferro());
