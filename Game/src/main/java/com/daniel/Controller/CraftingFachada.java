d68359ebe715961a56a1c3e3fa4466b7c8054ed0
Jacintones <thiago.jacinto@ufrpe.br>
2024-02-04 00:44:44 -0300

    Atualizaçao mvc

--- Game/src/main/java/com/daniel/Controller/CraftingFachada.java
+++ Game/src/main/java/com/daniel/Controller/CraftingFachada.java
@@ -1,5 +1,6 @@
 package com.daniel.Controller;
 
+import com.daniel.Model.Crafting;
 import com.daniel.Model.Exceptions.PlayerInexistenteException;
 import com.daniel.Model.Dados.Itens.Armas.*;
 import com.daniel.Model.Dados.Itens.Item;
@@ -12,8 +13,10 @@ import com.daniel.Model.Dados.Itens.Pocoes.PocaoMpGrande;
 public class CraftingFachada {
     private double chanceBase;
     private static CraftingFachada instance;
+    private Crafting crafting;
 
     private CraftingFachada() {
+        crafting= new Crafting();
     }
 
     public static CraftingFachada getInstance() {
@@ -23,140 +26,49 @@ public class CraftingFachada {
         return instance;
     }
     public Item criarBarraFerro(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) { //Verifica se nao é nulo
-            if (item instanceof Ferro && item2 instanceof Ferro) { //depois verifica se participam da condiçao específica do craft
-                BarraFerro barraFerro = new BarraFerro();
-                this.chanceBase = chance;
-                return barraFerro;
-            }
-        }
-        return null;
+        return crafting.criarBarraFerro(item, item2, chance);
     }
 
     public  Item criarPocaoVidaGrande(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof PocaoCura && item2 instanceof PocaoCura) {
-                PocaoCuraGrande pocaoCuraGrande = new PocaoCuraGrande();
-                this.chanceBase = chance;
-                return pocaoCuraGrande;
-            }
-        }
-        return null;
+        return crafting.criarPocaoVidaGrande(item, item2, chance);
     }
     public  Item criarPocaoManaGrande(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof PocaoMp && item2 instanceof PocaoMp) {
-                PocaoMpGrande pocaoMpGrande = new PocaoMpGrande();
-                this.chanceBase = chance;
-                return pocaoMpGrande;
-            }
-        }
-        return null;
+        return crafting.criarPocaoManaGrande(item, item2, chance);
     }
     public  Item criarBarraEscuridao(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof FragmentoEscuridao && item2 instanceof FragmentoEscuridao) {
-                BarraEscuridao barraEscuridao = new BarraEscuridao();
-                this.chanceBase = chance;
-                return barraEscuridao;
-            }
-        }
-        return null;
+        return crafting.criarBarraEscuridao(item, item2, chance);
     }
     public  Item criarBarraOuro(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof Ouro && item2 instanceof Ouro) {
-                BarraOuro barraOuro = new BarraOuro();
-                this.chanceBase = chance;
-                return barraOuro;
-            }
-        }
-        return null;
+        return crafting.criarBarraOuro(item, item2, chance);
     }
     public  Item criarBarraFogo(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof FragmentoFogo && item2 instanceof FragmentoFogo) {
-                BarraFogo barraFogo = new BarraFogo();
-                this.chanceBase = chance;
-                return barraFogo;
-            }
-        }
-        return null;
+        return crafting.criarBarraFogo(item, item2, chance);
     }
     public  Item criarBarraGelo(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof FragmentoGlacial && item2 instanceof FragmentoGlacial) {
-                BarraGlacial barraGlacial = new BarraGlacial();
-                this.chanceBase = chance;
-                return barraGlacial;
-            }
-        }
-        return null;
+        return crafting.criarBarraGelo(item, item2, chance);
     }
     public  Item criarFerroAperfeicoado(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof BarraFerro && item2 instanceof BarraFerro) {
-                FerroAperfeicoado ferroAperfeicoado = new FerroAperfeicoado();
-                this.chanceBase = chance;
-                return ferroAperfeicoado;
-            }
-        }
-        return null;
+        return crafting.criarFerroAperfeicoado(item, item2, chance);
     }
     public  Item criarEspadaAperfeicoada(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof EspadaInicial && item2 instanceof FerroAperfeicoado || item instanceof FerroAperfeicoado && item2 instanceof EspadaInicial) {
-                EspadaAperfeiçoada espadaAperfeiçoada = new EspadaAperfeiçoada();
-                this.chanceBase = chance;
-                return espadaAperfeiçoada;
-            }
-        }
-        return null;
+        return crafting.criarEspadaAperfeicoada(item, item2, chance);
     }
 
     public  Item criarEspadaGeloAperfeicoada(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof EspadaGeloBasica && item2 instanceof BarraGlacial || item instanceof BarraGlacial && item2 instanceof EspadaGeloBasica) {
-                EspadaGeloAperfeicoada espadaGeloAperfeicoada = new EspadaGeloAperfeicoada();
-                this.chanceBase = chance;
-                return espadaGeloAperfeicoada;
-            }
-        }
-        return null;
+        return crafting.criarEspadaGeloAperfeicoada(item, item2, chance);
     }
     public  Item criarEspadaEscuridaoAperfeicoada(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof EspadaSombriaBasica && item2 instanceof BarraEscuridao || item instanceof BarraEscuridao && item2 instanceof EspadaSombriaBasica) {
-                EspadaSombriaAperfeicoada espadaSombriaAperfeicoada = new EspadaSombriaAperfeicoada();
-                this.chanceBase = chance;
-                return espadaSombriaAperfeicoada;
-            }
-        }
-        return null;
+        return crafting.criarEspadaEscuridaoAperfeicoada(item, item2, chance);
     }
     public  Item criarEspadaFogoAperfeicoada(Item item, Item item2, double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof EspadaFogoBasica && item2 instanceof BarraFogo || item instanceof BarraFogo && item2 instanceof EspadaFogoBasica) {
-                EspadaDemoniaca espadaDemoniaca = new EspadaDemoniaca();
-                this.chanceBase = chance;
-                return espadaDemoniaca;
-            }
-        }
-        return null;
+        return crafting.criarEspadaFogoAperfeicoada(item, item2, chance);
     }
     public  Item criarEspadaEletricaAperfeicoada(Item item, Item item2,double chance) {
-        if (item != null && item2 != null) {
-            if (item instanceof EspadaEletricaBasica && item2 instanceof BarraOuro || item instanceof BarraOuro && item2 instanceof EspadaEletricaBasica) {
-                EspadaEletricaAperfeicoada espadaEletricaAperfeicoada = new EspadaEletricaAperfeicoada();
-                this.chanceBase = chance;
-                return espadaEletricaAperfeicoada;
-            }
-        }
-        return null;
+        return crafting.criarEspadaEletricaAperfeicoada(item, item2, chance);
     }
 
     public double getChanceBase() {
-        return chanceBase;
+        return crafting.getChanceBase();
     }
 
 }
--- Game/src/main/java/com/daniel/Controller/LojaFachada.java
+++ Game/src/main/java/com/daniel/Controller/LojaFachada.java
@@ -4,11 +4,13 @@ import com.daniel.Model.Dados.Entidades.Player;
 import com.daniel.Model.Exceptions.*;
 import com.daniel.Model.Interfaces.IEquipable;
 import com.daniel.Model.Dados.Itens.Item;
+import com.daniel.Model.Loja.Loja;
 
 public class LojaFachada {
     private static final LojaFachada instance = new LojaFachada();
-
+    private Loja loja;
     private LojaFachada() {
+        loja = new Loja();
     }
 
     public static LojaFachada getInstance() {
@@ -16,31 +18,11 @@ public class LojaFachada {
     }
 
     public void comprarItem( Item item) throws PlayerInexistenteException, RemoverCoinsException, CompraErroException, SemMoedasParaLojaException {
-        if (Player.getPlayer() != null && item != null) {
-            if (podeComprar(item)) {
-                // Realize a compra
-                Player.getPlayer().removerCoins(item.getPreco()); //remove o ouro
-                Player.getPlayer().getInventario().adicionarItem(item); // adiciona o item
-            } else {
-                throw new SemMoedasParaLojaException();
-            }
-        } else {
-            throw new CompraErroException();
-        }
+        loja.comprarItem(item);
     }
-    private boolean podeComprar(Item item) throws PlayerInexistenteException {
-        return Player.getPlayer().getCoins() >= item.getPreco();
-    }
+
     public void venderItem(Item item) throws PlayerInexistenteException {
-        if (item != null) {
-            if (item instanceof IEquipable) {
-                IEquipable equipableItem = (IEquipable) item;
-                Player.getPlayer().desequiparItemSeEquipado(Player.getPlayer(), equipableItem);
-            }
-            int precoItem = item.getPreco();
-            Player.getPlayer().ganhaCoins(precoItem * 70 / 100);
-            Player.getPlayer().getInventario().removerItem(item);
-        }
+        loja.venderItem(item);
     }
 
 
